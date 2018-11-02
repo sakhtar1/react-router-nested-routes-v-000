0dd137a93967beefa8d0e6d898089d93c66ced35
Sharmina Akhtar <sakht318@gmail.com>
2018-11-02 14:58:11 +0000

    automatically backed up by learn

---
+++ src/containers/MoviesNew.js
@@ -0,0 +1,45 @@
+//src/containers/MoviesNew
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { addMovie } from '../actions';
+ 
+class MoviesNew extends Component {
+ 
+  constructor() {
+    super();
+ 
+    this.state = {
+      title: ''
+    };
+  }
+ 
+  handleOnSubmit = event => {
+    event.preventDefault();
+    // Destructure addMovie and history from the components props
+    const { addMovie, history } = this.props;
+    // Create the movie with the Redux action
+    addMovie(this.state);
+    // redirect to /movies route
+    history.push('/movies')
+  }
+ 
+  handleOnChange = event => {
+    this.setState({
+      title: event.target.value
+    });
+  }
+ 
+  render(){
+    return (
+      <form style={{ marginTop: '16px' }} onSubmit={this.handleOnSubmit} >
+        <input 
+          type="text" 
+          onChange={this.handleOnChange} 
+          placeholder="Add a Movie" />
+        <input type="submit" value="Add Movie" />
+      </form>
+    );
+  }
+}
+ 
+export default connect(null, { addMovie })(MoviesNew)
